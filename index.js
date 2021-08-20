fdf5e858a843b46b584ed17cd03ceeeb3eac1637
Kevin <rizalarifin228@gmail.com>
2021-08-20 09:07:58 +0700

    first commit

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.on("ready", () => {
       let voice = bot.channels.cache.get("877638125462126683");
       voice.setName(timezone().tz("Asia/Jakarta").format("⌚ HH:mm [WIB]") + " ");
   }
-  setInterval(time, 8000);
+  setInterval(time, 10000);
 });
 
 bot.snipes = new Map();
