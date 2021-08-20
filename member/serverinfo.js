3df2c98251466adbf44b377c418d0b09b2ff45ff
Kevin <rizalarifin228@gmail.com>
2021-08-20 15:19:44 +0700

    first commit

--- member/serverinfo.js
+++ member/serverinfo.js
@@ -16,65 +16,66 @@ exports.run = async(bot, message, args) => {
         idle = message.guild.members.cache.filter(m => m.user.presence.status === "idle").size,
         offline = message.guild.members.cache.filter(m => m.user.presence.status === "offline").size;
   
-      let embed = new MessageEmbed()
+      let embed1 = new MessageEmbed()
         .setColor("YELLOW")
         .setTitle("**__SERVER INFO__**")
         .setThumbnail(thumbnail)
         .addField("owner name:", owner)
-      let embed1 = new MessageEmbed()
+      let embed2 = new MessageEmbed()
       .setColor("YELLOW")
       .addField("region:", region)
       .addField("server id:", Guild_Id)
       .addField("total member:", total)
       .addField("total channel:", totalChannel)
-      let embed2 = new MessageEmbed()
+      let embed3 = new MessageEmbed()
       .setColor("YELLOW")
       .addField("total emoji:", totalEmoji)
       .addField("total role:", role)
       .addField("member online", online)
       .addField("member dnd", dnd)
-      let embed3 = new MessageEmbed()
+      let embed4 = new MessageEmbed()
       .setColor("YELLOW")
       .addField("member idle:", idle)
       .addField("member offline", offline)
       .setTimestamp()
       .setFooter("Script by: BlueWolf#0371\n");
   
-new Menu(message.channel, message.author.id, [
-    {
-      name: "main",
-      content: embed,
-      reactions: {
-        "⚫": "stop",
-        "▶": "next"      
-      }
-    },
-    {
-      name: "otherPage",
-      content: embed1,
-      reactions: {
-        "⚫": "stop",
-        "↩": "previous",
-        "▶": "next"
-      }
-    },
-    {
-      name: "otherPage",
-      content: embed2,
-      reactions: {
-        "⚫": "stop",
-        "↩": "previous",
-        "▶": "next"
-      }
-    },
-    {
-      name: "otherPage",
-      content: embed3,
-      reactions: {
-        "⚫": "stop",
-        "↩": "previous"
-      }
-    }
-  ]);
-
+// new Menu(message.channel, message.author.id, [
+//     {
+//       name: "main",
+//       content: embed,
+//       reactions: {
+//         "⚫": "stop",
+//         "▶": "next"      
+//       }
+//     },
+//     {
+//       name: "otherPage",
+//       content: embed1,
+//       reactions: {
+//         "⚫": "stop",
+//         "↩": "previous",
+//         "▶": "next"
+//       }
+//     },
+//     {
+//       name: "otherPage",
+//       content: embed2,
+//       reactions: {
+//         "⚫": "stop",
+//         "↩": "previous",
+//         "▶": "next"
+//       }
+//     },
+//     {
+//       name: "otherPage",
+//       content: embed3,
+//       reactions: {
+//         "⚫": "stop",
+//         "↩": "previous"
+//       }
+//     }
+//   ]);
+const pages = [embed1, embed2, embed3, embed4];
+  ReactionPages(message, pages, false);
 }
