0afc192fb0e3d9f6ff058dd59cebd1e9c8949d7c
Kevin <rizalarifin228@gmail.com>
2021-08-21 20:35:58 +0700

    first commit

---
+++ moderator/lockdown.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "lockdown",
+    run: async(bot, message, args) => {
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You dont have permissions to usage this command!");
+
+        let channel = message.mentions.channels.first() ? message.mentions.channels.first() : message.channel;
+        if(!channel) return message.channel.send("Please specify a channel...");
+
+        try {
+            channel.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() === "@everyone"), {
+                SEND_MESSAGES: false
+            });
+            message.channel.send(`Successfully locked ${channel}`);
+        } catch(err) {
+            console.log(err);
+            return message.channel.send(`I couldn't lock ${channel}`);
+        }
+    }
+}
