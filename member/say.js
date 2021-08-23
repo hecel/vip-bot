620bb50194e640704b03d9bea400e04eda5e4ae4
Kevin <rizalarifin228@gmail.com>
2021-08-23 20:43:53 +0700

    first commit

--- member/say.js
+++ member/say.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: "say",
     run: async(bot, message, args) => {
+        await message.delete();
         let kata = args[1];
+        if(!kata) return message.channel.send("please specify the word");
         message.channel.send(kata);
     }
 }
