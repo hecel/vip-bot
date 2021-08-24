7cfa7e8a8528f156d4b4216c53ca5dda4f9b1620
Kevin <rizalarifin228@gmail.com>
2021-08-25 04:35:47 +0700

    first commit

---
+++ member/weather.js
@@ -0,0 +1,46 @@
+const weather = require("weather-js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "weather",
+    run: async(bot, message, args) => {
+        let wea = args[1];
+        //if(!wea) return message.channel.send("Please specify a weather");
+        weather.find({ search: wea, degreeType: `C` }, function(error, result) {
+            if(error) message.channel.send(error);
+
+            const embed = new MessageEmbed()
+            .setTitle("Please specify a location")
+            .setColor("PURPLE")
+            .setTimestamp();
+            if(!wea) return message.channel.send(embed);
+
+            const embed = new MessageEmbed()
+            .setTitle("Invalid location")
+            .setColor("PURPLE")
+            .setTimestamp();
+            if(result === undefined || result.length === 0) return message.channel.send(embed);
+
+            let current = result[0].current;
+            let location = result[0].location;
+
+            message.channel.send("Getting weather info...").then(m => {
+                
+                const embed = new MessageEmbed()
+                .setAuthor(`**current weather for ${current.observationpoint}**`)
+                .setThumbnail(current.imgurl)
+                .setDescription(`**${current.skytext}**`)
+                .addField(`Timezone:`, `UTC ${location.timezone}`, true)
+                .addField(`Degree Type:`, `celcius`, true)
+                .addField(`Temperature:`, `${current.temperature}°`, true)
+                .addField(`Wind:`, `${current.widdisplay}`, true)
+                .addField(`Feels Like:`, `${current.feelslike}`, true)
+                .addField(`Humidty:`, `${current.humidty}`, true)
+                .setColor("GREEN")
+                .setTimestamp();
+
+                m.edit(embed);
+            });
+        });
+    }
+}
