220a268204f6b2f003026c2fa4dd7283a18ca937
Kevin <rizalarifin228@gmail.com>
2021-08-24 11:49:47 +0700

    first commit

--- member/say.js
+++ member/say.js
@@ -4,6 +4,7 @@ module.exports = {
         await message.delete();
         let kata = args[1];
         if(!kata) return message.channel.send("please specify the word");
+        if(kata === "@everyone" || kata === "@here") return message.channel.send("You can't tag **everyone** and **here**");
         message.channel.send(kata);
     }
 }
