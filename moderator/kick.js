cd7c11e99cfb39ebcd875b76e103fad8d34c1232
Kevin <rizalarifin228@gmail.com>
2021-08-24 09:35:50 +0700

    first commit

--- moderator/kick.js
+++ moderator/kick.js
@@ -4,7 +4,7 @@ module.exports = {
         if(!message.member.hasPermission(["KICK_MEMBERS", "BAN_MEMBERS"]));
         const member = message.mentions.users.first();
         const memberTarget = message.guild.members.cache.get(member.id);
-        let rs = args[1];
+        let rs = args[2];
         if(!member) return message.channel.send("Please specify a member");
         if(!rs) return message.channel.send("Please specify a reason");
         memberTarget.kick({ reason: rs });
