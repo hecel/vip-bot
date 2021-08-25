7314a5732b9b2e81e7c8c9c14e4e47604df2374e
Kevin <rizalarifin228@gmail.com>
2021-08-25 12:40:22 +0700

    first commit

--- moderator/kick.js
+++ moderator/kick.js
@@ -4,7 +4,7 @@ module.exports = {
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You dont have permission to run this command!");
         const member = message.mentions.users.first();
         const memberTarget = message.guild.members.cache.get(member.id);
-        let rs = args[2];
+        let rs = args.slice(2).join(" ");
         if(!member) return message.channel.send("Please specify a member");
         if(!rs) return message.channel.send("Please specify a reason");
         memberTarget.kick(rs);
