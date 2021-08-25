bde1496b059fddc5042e753b76040f5276292e6a
Kevin <rizalarifin228@gmail.com>
2021-08-25 12:50:32 +0700

    first commit

---
+++ moderator/ban.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "kick",
+    run: async(bot, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You dont have permission to run this command!");
+        const member = message.mentions.users.first();
+        const memberTarget = message.guild.members.cache.get(member.id);
+        let rs = args.slice(2).join(" ");
+        if(!member) return message.channel.send("Please specify a member");
+        if(!rs) return message.channel.send("Please specify a reason");
+        memberTarget.ban({ reason: rs });
+        member.send(`You has been baned for reason: ${rs}`);
+        message.channel.send(`Successfully baned ${member} with reason: ${rs}`);
+    }
+}
