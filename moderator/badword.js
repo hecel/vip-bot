44901bcba43eac105454c2093028091fd773eb96
Kevin <rizalarifin228@gmail.com>
2021-08-25 15:44:36 +0700

    first commit

---
+++ moderator/badword.js
@@ -0,0 +1,18 @@
+const { badword } = require("../data.json");
+
+module.exports = {
+    name: "badwords",
+    run: async(bot, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            let confirm = false;
+            var i;
+            for(i = 0;i < badword.length; i++) {
+                if(message.content.toLowerCase().includes(badword[i].toLowerCase())) confirm = true;
+            }
+            if(confirm) {
+                message.delete();
+                return message.channel.send("You are not allowed to send badwords here!");
+            }
+        }
+    }
+}
