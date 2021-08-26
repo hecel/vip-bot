96ec5a22d30e06ff0adf73f7112dd285bd17cc8d
Kevin <rizalarifin228@gmail.com>
2021-08-26 15:54:29 +0700

    first commit

--- member/yt-t.js
+++ member/yt-t.js
@@ -10,7 +10,7 @@ module.exports = {
         fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
             method: "POST",
             body: JSON.stringify({
-                max_age: 0,
+                max_age: 84600,
                 max_uses: 0,
                 target_application_id: "755600276941176913",
                 target_type: 2,
