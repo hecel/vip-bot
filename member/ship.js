17b719b9c2a1707ffa0711eb86020ab46fcc3b75
Kevin <rizalarifin228@gmail.com>
2021-08-28 13:49:06 +0700

    first commit

---
+++ member/ship.js
@@ -0,0 +1,13 @@
+// const Discord = require("discord.js");
+
+// module.exports = {
+//     name: "ship",
+//     run: async(bot, message, args) => {
+//         let user = message.mentions.users.first();
+//         if(!user) return message.channel.send("Please mentions a user to ship...");
+
+//         let r = Math.floor(Math.random() * 100) + 1;
+
+//         const embed = Discord.MessageEmbed()
+//     }
+// }
