04c6ee121e5135598c499b5e9ee5a28b95d84490
Kevin <rizalarifin228@gmail.com>
2021-08-28 15:01:16 +0700

    first commit

--- member/ship.js
+++ member/ship.js
@@ -5,21 +5,22 @@ module.exports = {
     run: async(bot, message, args) => {
         let user = message.mentions.users.first();
         if(!user) return message.channel.send("Please mentions a user to ship");
+        if(user === message.author) return message.channel.send("Please mentions someone and not yourself");
 
         let r = Math.floor(Math.random() * 100) + 1;
 
-        const unLove = Discord.MessageEmbed()
+        const unLove = new Discord.MessageEmbed()
         .setTitle("This isn\'t a match")
         .setThumbnail("https://cdn.discordapp.com/attachments/824906735176253450/828554687229067275/images.png")
         .setDescription(`${message.author} shipped with ${user} and it is`)
-        .setColor("RANDOM")
+        .setColor("RED")
         .setTimestamp();
 
-        const love = Discord.MessageEmbed()
+        const love = new Discord.MessageEmbed()
         .setTitle("They are born for each others")
         .setThumbnail("https://cdn.discordapp.com/attachments/824906735176253450/828555115593859123/9k.png")
         .setDescription(`${message.author} shipped with ${user} and it is`)
-        .setColor("RANDOM")
+        .setColor("GREEN")
         .setTimestamp();
 
         if(r > 50) {
