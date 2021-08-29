f9df240cc050e0d0c9f7a052d5065ea2e015ceb0
Kevin <rizalarifin228@gmail.com>
2021-08-30 06:16:38 +0700

    first commit

--- member/translate.js
+++ member/translate.js
@@ -6,8 +6,8 @@ module.exports = {
     run: async(bot, message, args) => {
         let text = args.slice(2).join(" ");
         let lang = args[1];
-        if(!text) return message.channel.send("Please provide a text to translate");
-        if(!lang) return message.channel.send("Please provide a ISO code of the language");
+        if(!text) return message.channel.send("Please provide a text to translate").then(m => m.delete({ timeout: 400 }));
+        if(!lang) return message.channel.send("Please provide a ISO code of the language").then(m => m.delete({ timeout: 400 }));
 
         translate(text, { to: lang }).then(res => {
             const embed = new Discord.MessageEmbed()
@@ -18,7 +18,7 @@ module.exports = {
             .setTimestamp();
             message.channel.send(embed);
         }).catch(err => {
-            return message.channel.send("Please provide a ISO code of the language");
+            return message.channel.send("Please provide a ISO code of the language").then(m => m.delete({ timeout: 400 }));
         });
     }
 }
