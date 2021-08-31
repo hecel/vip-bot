669f6fde8d5efce31b444dc63536d0ac2bde3fd8
Kevin <rizalarifin228@gmail.com>
2021-09-01 06:50:29 +0700

    first commit

--- moderator/react.js
+++ moderator/react.js
@@ -6,18 +6,21 @@ module.exports = {
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You dont have permission **ADMINISTRATOR**");
 
         let roles = message.mentions.roles.first();
+        let emoji = args[2];
         if(!roles) return message.channel.send("Please mentions a roles to get added");
+        if(!emoji) return message.channel.send("Please specify a emoji");
 
+        //✅
         const embed = new Discord.MessageEmbed()
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
         .setColor("#00fdfd")
-        .setDescription(`React with ✅ to get the role ${roles}`)
+        .setDescription(`React with ${emoji} to get the role ${roles}`)
         .setTimestamp();
         let msg = await message.channel.send(embed);
 
-        await msg.react("✅");
+        await msg.react(emoji);
 
-        const filter = (reaction, user) => ["✅"].includes(reaction.emoji.name);
+        const filter = (reaction, user) => [emoji].includes(reaction.emoji.name);
         const collector = msg.createReactionCollector(filter, { dispose: true });
 
         collector.on("remove", (reaction, user) => {
