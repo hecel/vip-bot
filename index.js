ba5cb8c961e2e3981b9ac0b946834540d0edffea
Kevin <rizalarifin228@gmail.com>
2021-08-31 15:37:13 +0700

    first commit

--- index.js
+++ index.js
@@ -75,6 +75,24 @@ const antijoin = new Collection();
   //const command = require(`./commands/${file}`);
   //bot.commands.set(command.name, command);
 //}
+bot.on("guildCreate", (guild) => {
+    let ch;
+    guild.channels.cache.forEach((channel) => {
+        if(channel.type === "text" && !ch && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) ch = channel;
+    });
+    if(!ch) return;
+
+    const embed = new MessageEmbed()
+    .setAuthor(`Hello!, Thank you for inviting me to ${guild.name}`)
+    .setColor("#00fdfd")
+    .setDescription(`My prefix is **${prefix}**`)
+    .addField("need help?", "Take a look at my developers!")
+    .addField("join my developer's existing server!", "[join](https://discord.gg/MCZdCqJmqk)")
+    .setTimestamp();
+    ch.send(embed).catch(e => {
+        if(e) return;
+    });
+});
 bot.on("messageDelete", function(message, channel) {
     bot.snipes.set(message.channel.id, {
         content: message.content,
