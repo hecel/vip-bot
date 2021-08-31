88ca98fa009ec1998411fbc9ebbb6e496c240baf
Kevin <rizalarifin228@gmail.com>
2021-08-31 11:32:30 +0700

    first commit

--- member/hangman.js
+++ member/hangman.js
@@ -6,12 +6,13 @@ module.exports = {
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]);
         if(!channel) return message.channel.send("Please specify a channel.");
 
-        const word = args.slice(2).join(" ");
-        if(!word) return message.channel.send("Please specify a word to guess");
+        //const word = args.slice(2).join(" ");
+        //if(!word) return message.channel.send("Please specify a word to guess");
+        let w = ["dream", "sapnap", "george", "jorge", "PakGM", "ElestialHD", "Malik"];
 
         const hang = new hangman({
             message: message,
-            word: word,
+            word: random,
             client: bot,
             channelID: channel.id,
             permission: "MANAGE_MESSAGES"
