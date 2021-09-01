e4590c14ecc0324ab88269092d0803a7724d9fda
Kevin <rizalarifin228@gmail.com>
2021-09-02 06:04:24 +0700

    first commit

--- handler/module.js
+++ handler/module.js
@@ -6,12 +6,12 @@ table.setHeading('Command', 'Load status');
 
 module.exports = (bot) => {
   
-  readdirSync(`./commands/`).forEach(dir => {
+    readdirSync(`${process.cwd()}/commands/`).forEach(dir => {
       
-        const commands = readdirSync(`./commands/${dir}`).filter(file => file.endsWith(".js"));
+        const commands = readdirSync(`${process.cwd()}/commands/${dir}/`).filter(file => file.endsWith(".js"));
         
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`${process.cwd()}/commands/${dir}/${file}`);
     
             if (pull.name) {
                 bot.commands.set(pull.name, pull);
@@ -21,7 +21,7 @@ module.exports = (bot) => {
                 continue;
             }
     
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
+            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
 
