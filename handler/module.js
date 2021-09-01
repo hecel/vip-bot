f671423b2628c603e875b28a2e5f19616637a4b0
Kevin <rizalarifin228@gmail.com>
2021-09-01 21:10:24 +0700

    first commit

--- handler/module.js
+++ handler/module.js
@@ -6,12 +6,12 @@ table.setHeading('Command', 'Load status');
 
 module.exports = (bot) => {
   
-  readdirSync(`../commands/`).forEach(dir => {
+  readdirSync(`${process.cwd()}/commands/`).forEach(dir => {
       
-        const commands = readdirSync(`../commands/${dir}/`).filter(file => file.endsWith(".js"));
+        const commands = readdirSync(`${process.cwd()}/commands/${dir}/`).filter(file => file.endsWith(".js"));
         
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`${process.cwd()}/commands/${dir}/${file}`);
     
             if (pull.name) {
                 bot.commands.set(pull.name, pull);
