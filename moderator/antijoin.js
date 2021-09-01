4b1f0c21f84ae28006f4af58dbda82bbd14f26ad
Kevin <rizalarifin228@gmail.com>
2021-09-01 08:15:32 +0700

    first commit

--- moderator/antijoin.js
+++ moderator/antijoin.js
@@ -15,7 +15,7 @@ module.exports = {
                 m.delete({ timeout: 4000});
             });
 
-            getCollection.set(message.guild.id, []);
+            bot.antijoins.set(message.guild.id, []);
             message.channel.send("Turned on antijoin sytem.").then(m => {
                 m.delete({ timeout: 4000});
             });
@@ -24,7 +24,7 @@ module.exports = {
                 m.delete({ timeout: 4000});
             });
 
-            getCollection.delete(message.guild.id);
+            bot.antijoins.delete(message.guild.id);
             message.channel.send("Turned off antijoin sytem.").then(m => {
                 m.delete({ timeout: 4000 });
             });
