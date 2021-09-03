6ab8c46fa4aa1ec91ef9bcdaf5cee5ef5384efdf
Kevin <rizalarifin228@gmail.com>
2021-09-03 18:42:13 +0700

    first commit

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ bot.on("guildCreate", (guild) => {
     .setColor("#00fdfd")
     .setDescription(`My prefix is **${prefix}**`)
     .addField("need help?", "Take a look at my developers!")
-    .addField("join my developer's existing server!", "[join](https://discord.gg/zuVYHJfaHC)")
+    //.addField("join my developer's existing server!", "[join](https://discord.gg/zuVYHJfaHC)")
     .setTimestamp();
     ch.send(embed).catch(e => {
         if(e) return;
