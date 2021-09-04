f0656b717045e481f9bba77be1d024d057bcec62
Kevin <rizalarifin228@gmail.com>
2021-09-04 18:05:07 +0700

    first commit

--- handler/module.js
+++ handler/module.js
@@ -27,6 +27,9 @@ module.exports = (bot) => {
             //if (pull.premiums && Array.isArray(pull.premiums)) pull.premiums.forEach(premium => bot.premiums.set(premium, pull.name));
         }
     });
+    let channel = bot.channels.cache.get("883668517679927336");
 
-    console.log(table.toString());
+    if(!channel) return;
+
+    channel.send(table.toString());
 }
