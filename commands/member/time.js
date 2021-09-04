83c42413f6c2b143601d653e69e90b92c251fc4f
Kevin <rizalarifin228@gmail.com>
2021-09-05 03:11:23 +0700

    first commit

--- commands/member/time.js
+++ commands/member/time.js
@@ -7,7 +7,7 @@ module.exports = {
         try {
             const m = await message.channel.send("is setting the time...");
             setTimeout(() => {
-              const embed = new MessageEmbed()
+              const embed = new Discord.MessageEmbed()
                 .setColor("#FF8401")
                 .addField(`:flag_id: Time Zone Indonesian:\n**${timezone().tz("Asia/Jakarta").format("⌚️ HH:mm [WIB]") + " "}**\n**${timezone().tz("Asia/Irkutsk").format("⌚️ HH:mm [WITA]") + " "}**\n**${timezone().tz("Asia/Jayapura").format("⌚️ HH:mm [WIT]") + " "}**`, true)
                 .addField(`:flag_us: Time Zone America:\n**${timezone().tz("America/New_York").format("⌚️ HH:mm []") + " "}**\n**${timezone().tz("America/Los_Angeles").format("⌚️ HH:mm []") + " "}**`, true)
